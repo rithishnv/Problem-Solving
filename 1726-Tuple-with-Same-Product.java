e7ddef2728e72219f3aaf2f114c27e42a00ec746
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2025-02-06 10:11:11 +0530

    [1726] [Time Beats: 69.23%] [Memory Beats: 66.03%] - LeetPush

--- 1726-Tuple-with-Same-Product.java
+++ 1726-Tuple-with-Same-Product.java
@@ -1,23 +1,21 @@
 class Solution {
     public int tupleSameProduct(int[] nums) {
         Map<Integer,Integer> map=new HashMap<>();
-         int cnt=0;
-        for(int i=0;i<nums.length-1;i++){
+        for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
-                cnt+=8*map.getOrDefault(nums[i]*nums[j],0);
-                map.put(nums[i]*nums[j],map.getOrDefault(nums[i]*nums[j],0)+1);
-                // map.put(nums[i]*nums[j],li);
+                int li=map.getOrDefault(nums[i]*nums[j],0)+1;
+                map.put(nums[i]*nums[j],li);
             }
         }
-       
-        // for(int c:map.values()){
+        int cnt=0;
+        for(int c:map.values()){
             // int c=map.get(i).size()/2;
             // if(c>1){
-                // cnt+=c*(c-1)*4;
+                cnt+=c*(c-1)*4;
                 // cnt+=(c*(c-1)/2)*8;
             // }
             // cnt++;
-        // }
+        }
         // System.out.println(/4);
         return cnt;
     }
