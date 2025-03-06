88580a9bd78b3d1d0412f01d9e47328fd8b51531
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2025-03-06 20:50:30 +0530

    [2965] [Time Beats: 100.00%] [Memory Beats: 14.68%] - LeetPush

--- Find-Missing-and-Repeated-Values.java
+++ Find-Missing-and-Repeated-Values.java
@@ -1,22 +1,19 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int[] res=new int[2];
-        Set<Integer> set=new HashSet<>();
-        int n=grid.length;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(set.add(grid[i][j])){
-                    sum+=grid[i][j];
-                }
-                else
-                res[0]=grid[i][j];
-            }
+       int[] fre=new int[grid.length*grid[0].length+1];
+       int n=grid.length;
+       for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            fre[grid[i][j]]++;
         }
-        n*=n;
-        n=(n)*(n+1)/2;
-        n-=sum;
-        res[1]=n;
-        return res;
+       }
+       int[] res=new int[2];
+       for(int i=0;i<fre.length;i++){
+        if(fre[i]==0)
+        res[1]=i;
+        if(fre[i]>1)
+        res[0]=i;
+       }
+       return res;
     }
 }
