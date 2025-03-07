c2764d512033093ee9fca12453a07f3bda914315
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2025-03-07 22:14:07 +0530

    [2523] [Time Beats: 45.20%] [Memory Beats: 36.00%] - LeetPush

--- Closest-Prime-Numbers-in-Range.java
+++ Closest-Prime-Numbers-in-Range.java
@@ -1,23 +1,23 @@
 class Solution {
-    List<Integer> li=new ArrayList<>();
-    int prev=Integer.MAX_VALUE;
-    public boolean check(int n){
-        if(n==1)
-        return false;
-        if(n==2 ||n==3)
-        return true;
-        for(int i=2;i<=Math.sqrt(n);i++){
-            if(n%i==0)
-            return false;
-        }
-        return true;
-    }
     public int[] closestPrimes(int left, int right) {
+        boolean[] p=new boolean[right+1];
+        Arrays.fill(p,true);
+        List<Integer> li=new ArrayList<>();
+        int prev=Integer.MAX_VALUE;
+        p[0]=false;
+        p[1]=false;
         int[] res=new int[2];
         Arrays.fill(res,-1);
+        for(int i=2;i*i<=right;i++){
+            if(p[i]){
+            for(int j=i+i;j<=right;j+=i){
+                p[j]=false;
+            }
+            }
+        }
         for(int i=left;i<=right;i++){
-            if(check(i))
-            li.add(i);
+        if(p[i])
+        li.add(i);
         }
         for(int i=0;i<li.size()-1;i++){
             if(li.get(i+1)-li.get(i)<prev){
