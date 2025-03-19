0c21904fddd73e782f6e4d335a3b7f8a04836c66
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2025-03-19 21:03:06 +0530

    [3191] [Time Beats: 100.00%] [Memory Beats: 18.07%] - LeetPush

--- Minimum-Operations-to-Make-Binary-Array-Elements-Equal-to-One-I.java
+++ Minimum-Operations-to-Make-Binary-Array-Elements-Equal-to-One-I.java
@@ -1,15 +1,12 @@
 class Solution {
-    int flip(int[] nums,int i){
-        return (nums[i]==1)?0:1;
-    }
     public int minOperations(int[] nums) {
         int c=0;
         for(int i=0;i<nums.length-2;i++){
             if(nums[i]==0){
                 c++;
                 nums[i]=1;
-                nums[i+1]=flip(nums,i+1);
-                nums[i+2]=flip(nums,i+2);
+                nums[i+1]=1-nums[i+1];
+                nums[i+2]=1-nums[i+2];
             }
         }
         int n=nums.length;
