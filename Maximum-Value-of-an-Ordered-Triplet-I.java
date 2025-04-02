53bc09c66d449fc00165fff000b7629d88203660
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2025-04-02 14:15:15 +0530

    [2873] [Time Beats: 99.31%] [Memory Beats: 20.27%] - LeetPush

--- Maximum-Value-of-an-Ordered-Triplet-I.java
+++ Maximum-Value-of-an-Ordered-Triplet-I.java
@@ -1,14 +1,13 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
-        long max=0;
-        int n=nums.length;
-        for(int i=0;i<n-2;i++){
-            for(int j=i+1;j<n-1;j++){
-                for(int k=j+1;k<n;k++){
-                    max=Math.max(max,(long)(nums[i]-nums[j])*nums[k]);
-                }
-            }
+        long ans=0;
+        int diff=0;
+        int max=0;
+        for(int num:nums){
+            ans=Math.max(ans,(long)diff*num);
+            max=Math.max(max,num);
+            diff=Math.max(diff,max-num);
         }
-        return max;
+        return ans;
     }
 }
