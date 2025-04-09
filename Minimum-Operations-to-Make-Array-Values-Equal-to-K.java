78dc627e5b1395fe19fe813f2facae529bb91d4c
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2025-04-09 20:27:10 +0530

    [3375] [Time Beats: 82.71%] [Memory Beats: 43.73%] - LeetPush

---
+++ Minimum-Operations-to-Make-Array-Values-Equal-to-K.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        Set<Integer> set=new HashSet<>();
+        for(int i:nums){
+            if(i<k){
+            return -1;
+            }
+            if(i>k)
+            set.add(i);
+        }
+        return set.size();
+    }
+}
