b0edc96c0101101ba1b0dd35c4bd0a92838ae4fb
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2025-04-21 19:26:42 +0530

    [2145] [Time Beats: 33.33%] [Memory Beats: 44.44%] - LeetPush

---
+++ Count-the-Hidden-Sequences.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numberOfArrays(int[] differences, int lower, int upper) {
+       long s=0,min=0,max=0;
+       for(int i:differences){
+        s+=i;
+        min=Math.min(min,s);
+        max=Math.max(max,s);
+       }
+       return Math.max(0,(int)((upper-max)-(lower-min)+1));
+    }
+}
