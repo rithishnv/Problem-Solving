a258cb505bde75f4bcf7fe7d381eeb4cf09a6991
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2025-06-30 10:39:11 +0530

    [49] [Time Beats: 98.17%] [Memory Beats: 81.86%] - LeetPush

--- Group-Anagrams.java
+++ Group-Anagrams.java
@@ -1,18 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> map=new TreeMap<>();
+        Map<String,List<String>> map=new HashMap<>();
         for(String s:strs){
-            char[] arr=s.toCharArray();
-            Arrays.sort(arr);
+            char[] arr=new char[30];
+            for(char c:s.toCharArray()){
+                arr[c-'a']++;
+            }
             String k=String.valueOf(arr);
+            // System.out.println(k);
             List<String> li=map.getOrDefault(k,new ArrayList<String>());
             li.add(s);
             map.put(k,li);
         }
         List<List<String>> res=new ArrayList<>();
-        // for(String i:map.keySet()){
-        //     res.add(map.get(i));
-        // }
-        return new ArrayList(map.values());
+        for(String i:map.keySet()){
+            res.add(map.get(i));
+        }
+        return res;
     }
 }
