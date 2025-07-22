037497164db2243a9b0cf8e20c7d74a34a96df21
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2025-07-22 20:44:23 +0530

    [1695] [Time Beats: 97.88%] [Memory Beats: 74.55%] - LeetPush

--- Maximum-Erasure-Value.java
+++ Maximum-Erasure-Value.java
@@ -2,16 +2,16 @@ class Solution {
     public int maximumUniqueSubarray(int[] nums) {
         int i=0,j=0;
         int n=nums.length;
-        int[] arr=new int[10001];
+        boolean[] arr=new boolean[10001];
         int max=0,t=0;
         while(i<n){
-             while(j<n && arr[nums[i]]==1){
+             while(j<n && arr[nums[i]]){
                 t-=nums[j];
-                arr[nums[j]]--;
+                arr[nums[j]]=false;
                 j++;
             }
                 t+=nums[i];
-                arr[nums[i]]++;
+                arr[nums[i]]=true;
                 i++;
                 max=Math.max(t,max);
         }
