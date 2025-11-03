43a298db4792d00f0d367c807749fc01592ec453
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2025-11-03 21:08:49 +0530

    [1578] [Time Beats: 18.49%] [Memory Beats: 5.68%] - LeetPush

--- Minimum-Time-to-Make-Rope-Colorful.java
+++ Minimum-Time-to-Make-Rope-Colorful.java
@@ -1,19 +1,21 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
         int res=0;
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        // PriorityQueue<Integer> pq=new PriorityQueue<>();
+        int s=0,max=0;
         for(int i=0;i<colors.length()-1;i++){
             int j=i+1;
-            pq.add(neededTime[i]);
+            s=neededTime[i];
+            max=neededTime[i];
             while(j<colors.length()&&colors.charAt(i)==colors.charAt(j)){
-                pq.add(neededTime[j]);
+                s+=neededTime[j];
+                max=Math.max(neededTime[j],max);
                 j++;
             }
-            while(pq.size()>1){
-                res+=pq.poll();
-                i=j-1;
+            if(i!=j){
+            res+=(s-max);
+            i=j-1;
             }
-            pq.clear();
         }
         return res;
     }
