b4dad1c9f0a5e560357d9b314193f30752a4ff65
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2025-11-22 19:20:04 +0530

    [3190] [Time Beats: 100.00%] [Memory Beats: 18.77%] - LeetPush

---
+++ Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int c=0;
+        for(int i:nums){
+            if(i%3!=0)
+            c++;
+        }
+        return c;
+    }
+}
