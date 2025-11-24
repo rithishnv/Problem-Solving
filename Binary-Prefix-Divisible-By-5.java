f79d5e784f280e17e33058d4b279f6b3c7f60996
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2025-11-24 22:48:21 +0530

    [1018] [Time Beats: 21.31%] [Memory Beats: 25.70%] - LeetPush

---
+++ Binary-Prefix-Divisible-By-5.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> li=new ArrayList<>();
+        int sum=0;
+        for(int i:nums){
+            sum=(sum*2+i)%5;
+            li.add((sum==0)?true:false);
+        }
+        return li;
+    }
+}
