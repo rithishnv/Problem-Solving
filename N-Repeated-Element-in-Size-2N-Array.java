0633ba35a25b8fa18203b2c748f157edade76e6e
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2026-01-02 20:01:51 +0530

    [961] [Time Beats: 45.46%] [Memory Beats: 65.18%] - LeetPush

---
+++ N-Repeated-Element-in-Size-2N-Array.java
@@ -0,0 +1,12 @@
+1class Solution {
+2    public int repeatedNTimes(int[] nums) {
+3        int n=nums.length;
+4        int[] arr=new int[100001];
+5        for(int i:nums){
+6            arr[i]++;
+7            if(arr[i]==n/2)
+8            return i;
+9        }
+10        return 0;
+11    }
+12}
