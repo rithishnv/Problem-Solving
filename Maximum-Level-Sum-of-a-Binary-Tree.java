d0d4b7bf91caf570775a7dbfcb0214517f64bb1c
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2026-01-06 22:27:39 +0530

    [1161] [Time Beats: 93.82%] [Memory Beats: 56.35%] - LeetPush

--- Maximum-Level-Sum-of-a-Binary-Tree.java
+++ Maximum-Level-Sum-of-a-Binary-Tree.java
@@ -15,30 +15,28 @@
 15 */
 16class Solution {
 17    public int maxLevelSum(TreeNode root) {
-18        long max=Long.MIN_VALUE;
+18        int max=Integer.MIN_VALUE;
 19        int min=1;
 20        Queue<TreeNode> queue=new LinkedList<>();
 21        queue.add(root);
 22        int l=0;
 23        while(!queue.isEmpty()){
 24            int s=queue.size();
-25            long sum=0;
-26            int f=0;
-27            l++;
-28            for(int i=0;i<s;i++){
-29                TreeNode x=queue.poll();
-30                if(x==null)
-31                continue;
-32                f=1;
-33                sum+=x.val;
-34                queue.add(x.left);
-35                queue.add(x.right);
-36            }
-37            if(sum>max && f==1){
-38                max=sum;
-39                min=l;
-40            }
-41        }
-42        return min;
-43    }
-44}
+25            int sum=0;
+26            l++;
+27            for(int i=0;i<s;i++){
+28                TreeNode x=queue.poll();
+29                sum+=x.val;
+30                if(x.left!=null)
+31                queue.add(x.left);
+32                if(x.right!=null)
+33                queue.add(x.right);
+34            }
+35            if(sum>max){
+36                max=sum;
+37                min=l;
+38            }
+39        }
+40        return min;
+41    }
+42}
