78c10037965f6b972ff68d3b25f1805fed230cdf
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2026-01-20 21:36:12 +0530

    [3314] [Time Beats: 11.40%] [Memory Beats: 100.00%] - LeetPush

--- Construct-the-Minimum-Bitwise-Array-I.java
+++ Construct-the-Minimum-Bitwise-Array-I.java
@@ -1,18 +1,18 @@
-1    class Solution {
-2        public int[] minBitwiseArray(List<Integer> nums) {
-3            int a[]=new int[nums.size()];
-4            int k=0;
-5            for(int i=0;i<nums.size();i++){
-6                int f=0;
-7                for(int j=1;j<nums.get(i);j++){
-8                    if((j|(j+1))==nums.get(i)){
-9                        a[k++]=j;
-10                        f=1;
-11                        break;
-12                    }
-13                }
-14                if(f==0)a[k++]=-1;
-15            }
-16            return a;
-17        }
-18    }
+1class Solution {
+2    public int[] minBitwiseArray(List<Integer> nums) {
+3        int a[]=new int[nums.size()];
+4        int k=0;
+5        for(int i=0;i<nums.size();i++){
+6            int f=0;
+7            for(int j=1;j<nums.get(i);j++){
+8                if((j|(j+1))==nums.get(i)){
+9                     a[k++]=j;
+10                     f=1;
+11                     break;
+12                }
+13            }
+14            if(f==0)a[k++]=-1;
+15        }
+16        return a;
+17    }
+18}
