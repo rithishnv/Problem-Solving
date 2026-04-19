acb220d23c822ee855449ec711947bd1e4013e08
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2026-04-19 22:44:32 +0530

    [1855] [Time Beats: 98.64%] [Memory Beats: 90.51%] - LeetPush

--- Maximum-Distance-Between-a-Pair-of-Values.java
+++ Maximum-Distance-Between-a-Pair-of-Values.java
@@ -1,13 +1,16 @@
 1class Solution {
 2    public int maxDistance(int[] nums1, int[] nums2) {
 3        int i=0,j=0;
-4        int n=nums1.length,m=nums2.length;
-5        while(i<n && j<m){
-6            if(nums1[i]>nums2[j]){
-7                i++;
-8            }
-9           j++;
-10        }
-11        return Math.max(j-i-1,0);
-12    }
-13}
+4        int res=0;
+5        int n=nums1.length,m=nums2.length;
+6        while(i<n && j<m){
+7            if(nums1[i]<=nums2[j]){
+8                res=Math.max(res,j-i);
+9                j++;
+10            }
+11            else
+12             i++;
+13        }
+14        return res;
+15    }
+16}
