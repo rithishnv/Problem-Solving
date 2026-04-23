d8003347af5bfd42874bea1cbea7ae7d31f8a6d2
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2026-04-23 23:47:26 +0530

    [2615] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

--- Sum-of-Distances.java
+++ Sum-of-Distances.java
@@ -2,26 +2,20 @@
 2    public long[] distance(int[] nums) {
 3        Map<Integer,List<Integer>> map=new HashMap<>();
 4        int n=nums.length;
-5        long[] ans=new long[n];
+5        long[] res=new long[n];
 6        for(int i=0;i<n;i++){
 7            List<Integer>li=map.getOrDefault(nums[i],new ArrayList<Integer>());
 8            li.add(i);
 9            map.put(nums[i],li);
 10        }
-11        for (List<Integer> li : map.values()) {
-12            long sum = 0;
-13            for (int x : li) sum += x;
-14            long leftSum = 0;
-15            int m = li.size();
-16            for (int i = 0; i < m; i++) {
-17                long rightSum = sum - leftSum - li.get(i);
-18                long left  = (long) li.get(i) * i - leftSum;
-19                long right = rightSum - (long) li.get(i) * (m - i - 1);
-20                ans[li.get(i)] = left + right;
-21                leftSum += li.get(i);
-22            }
-23        }
-24
-25        return ans;
-26    }
-27}
+11        for(int i=0;i<n;i++){
+12            List<Integer>li=map.get(nums[i]);
+13            for(int j:li){
+14                if(j!=i){
+15                    res[i]+=Math.abs(i-j);
+16                }
+17            }
+18        }
+19        return res;
+20    }
+21}
