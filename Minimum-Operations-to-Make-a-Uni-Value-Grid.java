797b50f34b43e034cbdca368cdf86a844db2e767
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2026-04-28 20:00:08 +0530

    [2033] [Time Beats: 55.72%] [Memory Beats: 9.92%] - LeetPush

--- Minimum-Operations-to-Make-a-Uni-Value-Grid.java
+++ Minimum-Operations-to-Make-a-Uni-Value-Grid.java
@@ -1,23 +1,23 @@
-class Solution {
-    public int minOperations(int[][] grid, int x) {
-        int max=0;
-        List<Integer> li=new ArrayList<>();
-        int a=grid[0][0]%x;
-        for(int[] i:grid){
-            for(int j:i){
-                if(j%x!=a)
-                return -1;
-                li.add(j);
-            }
-        }
-        Collections.sort(li);
-        int t=li.get(li.size()/2);
-        int cnt=0;
-        for(int[] i:grid){
-            for(int j:i){
-                cnt+=Math.abs(j-t)/x;
-            }
-        }
-        return cnt;
-    }
-}
+1class Solution {
+2    public int minOperations(int[][] grid, int x) {
+3        int max=0;
+4        List<Integer> li=new ArrayList<>();
+5        int a=grid[0][0]%x;
+6        for(int[] i:grid){
+7            for(int j:i){
+8                if(j%x!=a)
+9                return -1;
+10                li.add(j);
+11            }
+12        }
+13        Collections.sort(li);
+14        int t=li.get(li.size()/2);
+15        int cnt=0;
+16        for(int[] i:grid){
+17            for(int j:i){
+18                cnt+=Math.abs(j-t)/x;
+19            }
+20        }
+21        return cnt;
+22    }
+23}
