82ae3c6b2fb8aaa55019facd29f54cde2ba7fc09
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2026-05-25 21:15:49 +0530

    [1871] [Time Beats: 92.54%] [Memory Beats: 59.33%] - LeetPush

--- Jump-Game-VII.java
+++ Jump-Game-VII.java
@@ -1,27 +1,23 @@
 1class Solution {
 2    public boolean canReach(String s, int minJump, int maxJump) {
 3        int n = s.length();
-4        Queue<Integer> q = new LinkedList<>();
-5        q.offer(0);
-6        int[] vis = new int[n];
-7        vis[0] = 1;
-8        int cur = 0;
-9        while (!q.isEmpty()) {
-10            int idx = q.poll();
-11            if (idx == n - 1) {
-12                return true;
-13            }
-14            int l = Math.max(cur , idx + minJump);
-15            int r = Math.min(idx + maxJump, n - 1);
-16
-17            for (int k = l; k <= r; k++) {
-18                if (s.charAt(k) == '0' && vis[k] == 0) {
-19                    vis[k] = 1;
-20                    q.offer(k);
-21                }
-22            }
-23            cur = Math.max(cur, r);
-24        }
-25        return false;
-26    }
-27}
+4        boolean[] vis = new boolean[n];
+5        vis[0]=true;
+6        int cur = 0;
+7        for(int idx=0;idx<n;idx++){
+8            if(!vis[idx]){
+9                continue;
+10            }
+11            int l = Math.max(cur+1 , idx + minJump);
+12            int r = Math.min(idx + maxJump, n - 1);
+13
+14            for (int k = l; k <= r; k++) {
+15                if (s.charAt(k) == '0' && !vis[k]) {
+16                    vis[k] = true;
+17                }
+18            }
+19            cur = Math.max(cur, r);
+20        }
+21        return vis[n-1];
+22    }
+23}
