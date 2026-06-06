74974036fef1aed107ea18b5d60e12640b8928a5
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2026-06-06 20:55:19 +0530

    [2574] [Time Beats: 98.84%] [Memory Beats: 68.25%] - LeetPush

--- Left-and-Right-Sum-Differences.java
+++ Left-and-Right-Sum-Differences.java
@@ -2,16 +2,15 @@
 2    public int[] leftRightDifference(int[] nums) {
 3      int lsum=0,rsum=0;
 4      int n=nums.length;
-5      for(int i=n-1;i>0;i--){
+5      for(int i=n-1;i>-1;i--){
 6        rsum+=nums[i];
 7      }
-8      for(int i=0;i<n-1;i++){
+8      for(int i=0;i<n;i++){
 9        int t=nums[i];
-10        nums[i]=Math.abs(lsum-rsum);
-11        lsum+=t;
-12        rsum-=nums[i+1];
+10        rsum-=nums[i];
+11        nums[i]=Math.abs(lsum-rsum);
+12        lsum+=t;
 13      }  
-14      nums[n-1]=Math.abs(lsum-rsum);
-15      return nums;
-16    }
-17}
+14      return nums;
+15    }
+16}
