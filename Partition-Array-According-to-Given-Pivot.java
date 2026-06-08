6795974659f76bd06a53dd31c4a191f6fe9203f1
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2026-06-08 20:08:37 +0530

    [2161] [Time Beats: 73.56%] [Memory Beats: 26.79%] - LeetPush

--- Partition-Array-According-to-Given-Pivot.java
+++ Partition-Array-According-to-Given-Pivot.java
@@ -1,23 +1,23 @@
-class Solution {
-    public int[] pivotArray(int[] arr, int pivot) {
-        int n=arr.length;
-        int[] nums=new int[n];
-        int j=0;
-        for(int i=0;i<n;i++){
-            if(arr[i]<pivot){
-                nums[j++]=arr[i];
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(arr[i]==pivot){
-                nums[j++]=arr[i];
-            }
-        }
-         for(int i=0;i<n;i++){
-            if(arr[i]>pivot){
-                nums[j++]=arr[i];
-            }
-        }
-        return nums;
-    }
-}
+1class Solution {
+2    public int[] pivotArray(int[] arr, int pivot) {
+3        int n=arr.length;
+4        int[] nums=new int[n];
+5        int j=0;
+6        for(int i=0;i<n;i++){
+7            if(arr[i]<pivot){
+8                nums[j++]=arr[i];
+9            }
+10        }
+11        for(int i=0;i<n;i++){
+12            if(arr[i]==pivot){
+13                nums[j++]=arr[i];
+14            }
+15        }
+16         for(int i=0;i<n;i++){
+17            if(arr[i]>pivot){
+18                nums[j++]=arr[i];
+19            }
+20        }
+21        return nums;
+22    }
+23}
