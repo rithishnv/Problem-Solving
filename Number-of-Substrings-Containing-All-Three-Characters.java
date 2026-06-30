06d02f0cced17d225217e3bd487ebdba6e8b273e
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2026-06-30 19:25:37 +0530

    [1358] [Time Beats: 99.09%] [Memory Beats: 45.69%] - LeetPush

--- Number-of-Substrings-Containing-All-Three-Characters.java
+++ Number-of-Substrings-Containing-All-Three-Characters.java
@@ -1,18 +1,18 @@
-class Solution {
-    public int numberOfSubstrings(String s) {
-        int res=0,i=0,j=0,c=0;
-        int[] arr=new int[3];
-        while(i<s.length()){
-            if(arr[s.charAt(i)-'a']++==0)
-            c++;
-            while(c>=3){
-                res+=s.length()-i;
-                if(--arr[s.charAt(j)-'a']==0)
-                c--;
-                j++;
-            }
-            i++;
-        }
-        return res;
-    }
-}
+1class Solution {
+2    public int numberOfSubstrings(String s) {
+3        int res=0,i=0,j=0,c=0;
+4        int[] arr=new int[3];
+5        while(i<s.length()){
+6            if(arr[s.charAt(i)-'a']++==0)
+7            c++;
+8            while(c>=3){
+9                res+=s.length()-i;
+10                if(--arr[s.charAt(j)-'a']==0)
+11                c--;
+12                j++;
+13            }
+14            i++;
+15        }
+16        return res;
+17    }
+18}
