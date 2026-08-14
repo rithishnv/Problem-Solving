82f215fedd4a24453a05bb74cfef31a66f1d031e
Rithish NV <165628971+rithishnv@users.noreply.github.com>
2026-08-14 22:11:23 +0530

    [3090] [Time Beats: 100.00%] [Memory Beats: 79.53%] - LeetPush

--- 3090-Maximum-Length-Substring-With-Two-Occurrences.java
+++ 3090-Maximum-Length-Substring-With-Two-Occurrences.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
-         Map<Character,Integer> map=new HashMap<>();
+        int[] arr=new int[26];
         int res=0;
         int i=0;
         int n=s.length();
         for(int j=0;j<n;j++){
-            map.put(s.charAt(j),map.getOrDefault(s.charAt(j),0)+1);
-            while(map.get(s.charAt(j))>2){
-                map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)-1);
+            arr[s.charAt(j)-'a']++;
+            while(arr[s.charAt(j)-'a']>2){
+                arr[s.charAt(i)-'a']--;
                 i++;
             }
             res=Math.max(res,j-i+1);
